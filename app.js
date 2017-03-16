61c10aa77a916ec83333f42bed667aeac9e7941b
lingzi2016 <380500350@qq.com>
2017-03-16 13:46:50 +0800

    create

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('view engine','html');
+
+app.set('views',path.join(__dirname));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/',function(req,res){
+    var indexPath = path.join(__dirname,'index.html');
+    res.sendFile(indexPath);
+});
+
+var server = app.listen(8080,function(){
+    console.log('server is started at:127.0.0.1:8080');
+});
