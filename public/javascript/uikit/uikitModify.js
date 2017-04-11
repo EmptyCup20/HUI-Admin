64563cb11b464a0ee893bfe71668f6828ac7bd63
lingzi2016 <380500350@qq.com>
2017-04-11 17:43:52 +0800

    update

--- public/javascript/uikit/uikitModify.js
+++ public/javascript/uikit/uikitModify.js
@@ -2,6 +2,28 @@
  * Created by zhengjunling on 2017/3/27.
  */
 define(["mdeditor"], function (Mdeditor) {
+    //动效信息
+    var Uikit = Backbone.Model.extend({
+        urlRoot: window.App.apiIp + "/admin/uikit",
+        defaults: {
+            content: "",
+            attachment_url: null,
+            attachment_name: null
+        },
+
+        validate: function (attrs) {
+            if (!$.trim(attrs.content)) {
+                return "内容不能为空";
+            }
+        },
+
+        update: function () {
+            this.fetch({
+                reset: true
+            });
+        }
+    });
+
     return Backbone.View.extend({
         initialize: function () {
             var that = this;
@@ -12,24 +34,39 @@ define(["mdeditor"], function (Mdeditor) {
         },
 
         render: function () {
-            $(".page").html(this.template);
+            this.model = new Uikit();
+
+            this.model.on("reset change", $.proxy(this.renderPage, this));
+
+            this.model.on("invalid", function (model, error) {
+                alertify.error(error);
+            });
+
+            this.model.update();
+        },
+
+        renderPage: function () {
+            var that = this;
+            $(".page").html(_.template(this.template)(this.model.attributes));
             this.setElement("#uikitModify");
 
             this.mdeditor = new Mdeditor("#uikitEditor");
 
             this.mdeditor.setOption({
-                url: window.App.apiIp + "/admin/uikit/modify",
-
                 uploadUrl: window.App.apiIp + "/admin/upload/fileUpload",
+                change: function (content) {
+                    that.model.set({content: content}, {silent: true});
+                },
 
-                uploadFieldName: "uikitImgUpload",
-
-                validate: function (content) {
-                    if (!$.trim(content)) {
-                        alertify.error("内容不能为空");
-                        return false;
-                    }
-                    return true;
+                submit: function () {
+                    that.model.save(that.model.attributes, {
+                        success: function () {
+                            alertify.success("保存成功");
+                        },
+                        error: function () {
+                            alertify.error("保存失败，请重试！");
+                        }
+                    });
                 },
 
                 done: function (res) {
@@ -39,9 +76,33 @@ define(["mdeditor"], function (Mdeditor) {
                 fail: function () {
                     alertify.error("保存失败，请重试");
                 }
-            });
+            }).setValue(this.model.get("content"));
 
-            this.getContent();
+            //this.getContent();
+            this.initUpload();
+        },
+
+        initUpload: function () {
+            var that = this;
+            //附件上传
+            this.$("[name=attachmentFile]").fileupload({
+                url: window.App.apiIp + "/admin/upload/fileUpload",
+                formData: {
+                    name: "attachmentFile"
+                },
+                done: function (t, result) {
+                    var res = result.result;
+                    if (res.success) {
+                        alertify.success(res.message);
+                        that.model.set({
+                            attachment_url: res.data.downloadUrl,
+                            attachment_name: res.data.name
+                        });
+                    } else {
+                        alertify.error(res.message);
+                    }
+                }
+            });
         },
 
         /**
